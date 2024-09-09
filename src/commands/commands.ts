a3a7049f12f0d0feda18d7d22e6cee7956166791
Pulkit Singh <pulkitsingh01@gmail.com>
2024-09-09 09:52:31 +0530

    clear chat history command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,3 +1,4 @@
+import { ChatRepository } from "../repositories/chat.respository";
 import * as vscode from "vscode";
 
 // Define an array of commands with their corresponding callback functions
@@ -9,6 +10,22 @@ export const commands = [
       vscode.window.showInformationMessage(
         "Hello World from creator-extension!"
       );
+    },
+  },
+  {
+    commandId: "creator-extension.clearChatHistory",
+    callback: async () => {
+      // Implement the logic to clear the chat history
+      const activeChat = await ChatRepository.getActiveChat();
+      if (activeChat) {
+        activeChat.messages = []; // Clear the messages array
+        await ChatRepository.updateChat(activeChat);
+
+        // Refresh the chat view
+        // You might need to access the webviewView instance and call webviewView.webview.postMessage
+        // to trigger an update in the chat view's content
+        // (This would typically be done in the chat-view.ts file where you have access to the webviewView)
+      }
     },
   },
   // Add more commands as needed
